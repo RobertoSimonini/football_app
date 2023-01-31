bc0e2b1a934205db00d375513dc457662fdf1b2b
RobertoSimonini <robertosimonini1@gmail.com>
2023-01-31 17:59:19 +0100

    created single math card component and moved logic there

---
+++ src/components/MatchCard.vue
@@ -0,0 +1,48 @@
+<script>
+export default {
+    props: {
+        match: Object
+    }
+}
+</script>
+
+
+<template>
+    <!-- Qui metto l'intere informazioni relative al singolo match  -->
+
+    <div class="match-container d-flex flex-column text-white align-items-center justify-content-center my-3">
+
+        <!-- Qui metto solo le squadre -->
+        <div class="teams d-flex align-items-center">
+            <div class="home-team d-flex align-items-center">
+                <img :src="match.homePic" alt="">
+                <h2> {{ match.homeTeam }}</h2>
+            </div>
+
+                <h1 class="px-3"> VS </h1>
+
+            <div class="transfer-team d-flex align-items-center">
+                <h2> {{ match.transferTeam }} </h2>
+                <img :src="`${match.transferPic}`" alt="">
+            </div>
+        </div>
+
+        <!-- Qui metto la data e lo stadio dove si giocherà la partita  -->
+        <div class="other-info w-100">
+            <div class="date d-flex justify-content-center align-items-center fw-bold">
+                <i class="fa-solid fa-calendar-days text-secondary pe-1"></i>
+                <span class="pe-3"> {{match.date}} </span>
+
+                <i class="fa-solid fa-circle-plus text-secondary pe-1"></i>
+                <span> {{ match.stadium }} </span>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+
+
+<style scoped>
+
+</style>
--- src/components/UpComingMatch.vue
+++ src/components/UpComingMatch.vue
@@ -1,4 +1,5 @@
 <script>
+import MatchCard from './MatchCard.vue';
 export default {
     data (){
         return {
@@ -45,7 +46,8 @@ export default {
                 },
             ]
         }
-    }
+    },
+    components: {MatchCard}
 }
 
 </script>
@@ -57,36 +59,7 @@ export default {
     <h2 class="text-white">
         Upcoming Matches
     </h2>
-
-    <!-- Qui metto l'intere informazioni relative al singolo match  -->
-    <div v-for="match in matches" class="match-container d-flex flex-column text-white align-items-center justify-content-center my-3">
-
-        <!-- Qui metto solo le squadre -->
-        <div class="teams d-flex align-items-center">
-            <div class="home-team d-flex align-items-center">
-                <img :src="match.homePic" alt="">
-                <h2> {{ match.homeTeam }}</h2>
-            </div>
-    
-                <h1 class="px-3"> VS </h1>
-    
-            <div class="transfer-team d-flex align-items-center">
-                <h2> {{ match.transferTeam }} </h2>
-                <img :src="`${match.transferPic}`" alt="">
-            </div>
-        </div>
-
-        <!-- Qui metto la data e lo stadio dove si giocherà la partita  -->
-        <div class="other-info w-100">
-            <div class="date d-flex justify-content-center align-items-center fw-bold">
-                <i class="fa-solid fa-calendar-days text-secondary pe-1"></i>
-                <span class="pe-3"> {{match.date}} </span>
-
-                <i class="fa-solid fa-circle-plus text-secondary pe-1"></i>
-                <span> {{ match.stadium }} </span>
-            </div>
-        </div>
-    </div>
+    <match-card v-for="match in matches" :match="match"></match-card>    
 
 </section>
 
