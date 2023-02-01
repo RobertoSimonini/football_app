74f03eff5d2d3edf3a4d5b96657ec688920eae8f
RobertoSimonini <robertosimonini1@gmail.com>
2023-02-01 15:59:38 +0100

    Added sponsors

--- src/components/AppSponsor.vue
+++ src/components/AppSponsor.vue
@@ -1,19 +1,53 @@
 <script>
 export default {
-
+    data(){
+        return {
+            sponsors: [
+                {
+                    url: '/src/assets/img/client2.png',
+                    id: 1
+                },
+                {
+                    url: '/src/assets/img/client3.png',
+                    id: 2
+                },
+                {
+                    url: '/src/assets/img/client4.png',
+                    id: 3
+                },
+                {
+                    url: '/src/assets/img/client7.png',
+                    id: 4
+                },
+                {
+                    url: '/src/assets/img/client9.png',
+                    id: 1
+                },
+                {
+                    url: '/src/assets/img/client10.png',
+                    id: 1
+                },
+            ]
+        }
+    }
 }
 
 </script>
 
 
 <template>
-    <section id="sponsor">
-        <div class="sponsor-top">
-            <figure>
-                <img src="../assets/img/reklam.jpg" alt="">
-            </figure>
-        </div>
-    </section>
+        <section id="sponsor">
+            <div class="sponsor-top">
+                <figure class="h-100">
+                    <img class="h-100" src="../assets/img/reklam.jpg" alt="">
+                </figure>
+            </div>
+            <div class="sponsor-bottom d-flex justify-content-between align-items-center px-5">
+                <figure v-for="sponsor in sponsors" :key="sponsor.id">
+                    <img role="button" :src="sponsor.url" alt="">
+                </figure>
+            </div>  
+        </section>
 
 </template>
 
@@ -28,13 +62,26 @@ export default {
         min-height: 300px;
 
         .sponsor-top {
-            height: 150px;
-
+            height: 275px;
             img {
                 object-fit: cover;
                 height: 100%;
                 width: 100%;
             }
+        }
+
+        .sponsor-bottom {
+            height: 275px;
+            background-color: #000000;
+            color: #fff;
+
+            img {
+                height: 200px;
+                border: 2px solid #ffff;
+                padding: 1.5rem;
+                border-radius: 50px;
+            }
+
         }
     }
 </style>
