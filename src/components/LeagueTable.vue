f3c767d86656ec862e7f87ebe260d426ffdabf80
RobertoSimonini <robertosimonini1@gmail.com>
2023-02-01 12:23:49 +0100

    added results and games dinamically

--- src/components/LeagueTable.vue
+++ src/components/LeagueTable.vue
@@ -1,12 +1,80 @@
 <script>
 export default {
-    
+    data() {
+        return {
+            matches: [
+                {
+                    homeTeam: 'Istanbul Sports',
+                    homePic: '/src/assets/img/league-club-1.png',
+                    transferTeam: 'United Fs Club',
+                    transferPic: '/src/assets/img/league-club-2.png',
+                    id: 1
+                },
+                {
+                    homeTeam: 'Schalke Club',
+                    homePic: '/src/assets/img/league-club-3.png',
+                    transferTeam: 'Inter Club',
+                    transferPic: '/src/assets/img/league-club-4.png',
+                    id: 2
+                },
+                {
+                    homeTeam: 'Madrid Club',
+                    homePic: '/src/assets/img/league-club-5.png',
+                    transferTeam: 'Schalke Club',
+                    transferPic: '/src/assets/img/league-club-6.png',
+                    id: 3  
+                },
+                {
+                    homeTeam: 'Arsenal Club',
+                    homePic: '/src/assets/img/league-club-7.png',
+                    transferTeam: 'Bayern Club',
+                    transferPic: '/src/assets/img/league-club-8.png',
+                    id: 4
+                },
+                {
+                    homeTeam: 'Atletico Club',
+                    homePic: '/src/assets/img/league-club-9.png',
+                    transferTeam: 'Istanbul Sports',
+                    transferPic: '/src/assets/img/league-club-1.png',
+                    id: 5
+                },
+                {
+                    homeTeam: 'Unite Fs Club',
+                    homePic: '/src/assets/img/league-club-2.png',
+                    transferTeam: 'Schalke Club',
+                    transferPic: '/src/assets/img/league-club-3.png',
+                    id: 5
+                },
+                {
+                    homeTeam: 'Inter Club',
+                    homePic: '/src/assets/img/league-club-4.png',
+                    transferTeam: 'Madrid Club',
+                    transferPic: '/src/assets/img/league-club-5.png',
+                    id: 5
+                },
+                {
+                    homeTeam: 'Schalke Club',
+                    homePic: '/src/assets/img/league-club-6.png',
+                    transferTeam: 'Arsenal Club',
+                    transferPic: '/src/assets/img/league-club-7.png',
+                    id: 5
+                },
+                {
+                    homeTeam: 'Bayern Club',
+                    homePic: '/src/assets/img/league-club-8.png',
+                    transferTeam: 'Atletico Club',
+                    transferPic: '/src/assets/img/league-club-9.png',
+                    id: 5
+                },
+            ]
+        }
+    }   
 }
 
 </script>
 
 <template>
-    <section id="ratings" class="p-5">
+    <section id="ratings" class="p-5 d-flex justify-content-between flex-wrap">
         <table class="table table-borderless">
             <thead>
                 <tr>
@@ -130,18 +198,37 @@ export default {
 
             <tfoot>
                 <tr class="text-center">
-                    <th scope="col" class="w-100">
-                        View Full League Table
-                    </th>
+                    <th scope="col" class="w-100">View full League Table</th> <th scope="col"></th>
+                    <th scope="col"></th>
+                    <th scope="col"></th>
+                    <th scope="col"></th>
                 </tr>
             </tfoot>
         </table>
+
+        <table id="results" class="table table-striped">
+            <tbody>
+                <tr v-for="match in matches" class="text-center">
+                    <td>
+                        <img :src="match.homePic" alt="">
+                        <span class="fw-normal">{{match.homeTeam}} </span>
+                        <span class="px-2">VS</span>
+                        <span class="fw-normal">{{match.transferTeam}}</span>
+                        <img :src="match.transferPic" alt="">
+                    </td>
+                   
+                </tr>
+            </tbody>
+        </table>
+
     </section>
 
 </template>
 
 
 <style lang="scss" scoped>
+
+ //TABELLA
 
 #ratings {
     min-height: 500px;
@@ -166,6 +253,45 @@ export default {
                 vertical-align: middle;
             }
         }
+}
+
+// Qui gestisco i colori in base alla sezione della tabella 
+
+thead {
+    background-color: #212121;
+    color: #ffff;
+
+    th {
+        font-weight: bold;
+        font-size: 20px;
+    }
+
+}
+
+tbody {
+    background-color: #F5F5F5;
+}
+
+tfoot {
+    background-color: #000000;
+    color: #ffff;
+    height: 65px;
+    font-weight: bold;
+}
+
+// RESULTS 
+
+#results {
+    width: 45%;
+
+    img {
+        height: 50px;
+    }
+
+    .teams {
+        line-height: 75px;
+    }
+
 }
 
 </style>
